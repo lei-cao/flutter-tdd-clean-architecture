08e46396a45b660a6c9debccb4384b5055a0b425
Lei Cao <lei@remerge.io>
2021-05-30 23:43:01 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/lei_cao/flutter_tdd_clean_architecture/flutter_tdd_clean_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lei_cao.flutter_tdd_clean_architecture.flutter_tdd_clean_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
